f7d492f571ecd77a4b2e698532421757dde48816
Ryan Weast <ryweast@gmail.com>
2025-08-29 09:59:17 -0400

    Switch log to template

--- server/build.sh
+++ server/build.sh
@@ -24,6 +24,7 @@ aws s3 cp s3://$S3_NAME/$REGISTRATION_S3_KEY flex-gateway/conf/registration.yaml
 export REDIS_ADDRESS=$(aws ssm get-parameter --name "/$APP_NAME/$ENV_NAME/redis_endpoint" --query "Parameter.Value" --output text)
 export REDIS_PW=$(aws ssm get-parameter --name "/$APP_NAME/$ENV_NAME/redis_pw" --with-decryption --query "Parameter.Value" --output text)
 envsubst <flex-gateway/templates/redis-config-template.yaml >flex-gateway/conf/redis-config.yaml
+envsubst <flex-gateway/templates/logs-config-template.yaml >flex-gateway/conf/logs-config.yaml
 # Run all these commands as ec2-user (required because it establishes new docker group)
 sudo -u ec2-user --preserve-env=APP_NAME,ENV_NAME -i <<'EOF'
 cd ~/mulesoft-iac/server/flex-gateway
