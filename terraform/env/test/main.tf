d4c9d1eeebd1138313c579ba485d831f34fe8227
Ryan Weast <ryweast@gmail.com>
2025-12-02 13:51:59 -0500

    Migrate TF State to S3 (#51)
    * Update renovate config
    * Migrate TF State to S3
    * Use different AWS source keys

--- terraform/env/test/main.tf
+++ terraform/env/test/main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = "~> 1.12"
 
-  cloud {
-    organization = "Philadelphia"
-
-    workspaces {
-      name = "mulesoft-flex-gateway-test"
-    }
+  backend "s3" {
+    bucket = "phl-citygeo-terraform-state"
+    # CHANGE ME!
+    key          = "flex-gateway/test"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 
   required_providers {
@@ -23,6 +23,11 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::975050025792:role/TFRole"
+    session_name = "tf"
+  }
 }
 
 provider "secretsmanager" {
